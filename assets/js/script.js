fc07c044e4e160a1a2654a6a0edd22b23567105c
KaterinMeyer <katerinmeyer@gmail.com>
2023-01-27 20:25:27 -0300

    Incorporación de Botones Actualizar y eliminar
    Se avance en proyecto.
    Se incluyen botones de actualizar y eliminar.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 let form = document.getElementById("form");
 let input = document.getElementById("input")
 let data = [];
+const DataKeys = 'data'
+let DataList = document.getElementById("lista-datos")
+let CrearBotonUpdateData = document.createElement('button')
 
 
 //Validador que usuario esta ingresando un contenido en el casillero. Si no es asi, deja un mensaje. 
@@ -18,17 +21,58 @@ let formValidation = () => {
     } else {
         console.log("successs");
         aviso.innerHTML = "";
-        AgregarData();
+        AddData();
+
     }
 };
 
-//Agregar data
+//Agregar data al arreglo data
 
-let AgregarData = () => {
+let AddData = () => {
+
     data.push(input.value);
-
     localStorage.setItem('data', JSON.stringify(data));
-    console.log(data);
+    GetData();
 };
+
+let UpdateData = (index) => {
+    let newName = document.getElementById('lista-datos').value
+    data[index] = newName
+    localStorage.setItem(DataKeys, JSON.stringify(data))
+    //window.location.reload()
+}
+const deleteContact = ( index ) => {
+  data.splice( index, 1 )
+  localStorage.setItem( DataKeys, JSON.stringify(data))
+  window.location.reload()
+}
+
+let GetData = () => {
+    const DataListString = localStorage.getItem(DataKeys)
+    data = JSON.parse(DataListString)
+    console.log(data);
+    //DataList.innerHTML = "";
+
+    data.forEach((data, index) => {
+        const nodeName = document.createElement('p')
+        const updateButton = document.createElement('button')
+        updateButton.textContent = `Actualizar ${index + 1}`
+        updateButton.setAttribute('onclick', `UpdateData(${index})`)
+
+
+        const deleteButton = document.createElement('button')
+        deleteButton.textContent = `Eliminar ${index + 1}`
+
+        deleteButton.setAttribute('onclick', `deleteContact(${index})`)
+        nodeName.textContent = `${index + 1}: ${data}`
+        DataList.appendChild(nodeName)
+        DataList.appendChild(updateButton)
+        DataList.appendChild(deleteButton)
+    });
+};
+
+
+// Cargar los datos del localstorage
+
 
 
