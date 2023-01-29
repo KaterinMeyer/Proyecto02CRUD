bcc283fba1b671bf24036b07be505411b00a3b7e
KaterinMeyer <katerinmeyer@gmail.com>
2023-01-29 17:31:37 -0300

    Se agrega funcion de eliminar, corrección general

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,16 @@ let form = document.getElementById("form");
 let input = document.getElementById("input")
 let data = [];
 const DataKeys = 'data'
-let DataList = document.getElementById("lista-datos")
+let DataList = document.getElementById("ListaDatos")
 let CrearBotonUpdateData = document.createElement('button')
 
+function CleanDataList(){
+	get("#ListaDatos").innerHTML = '';
+}
 
 //Validador que usuario esta ingresando un contenido en el casillero. Si no es asi, deja un mensaje. 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("button clicked");
-
     formValidation();
 });
 
@@ -32,47 +33,63 @@ let AddData = () => {
 
     data.push(input.value);
     localStorage.setItem('data', JSON.stringify(data));
-    GetData();
+    UpdateDataList();
 };
 
-let UpdateData = (index) => {
-    let newName = document.getElementById('lista-datos').value
-    data[index] = newName
-    localStorage.setItem(DataKeys, JSON.stringify(data))
-    //window.location.reload()
+const UpdateData = () => {
+    document.getElementById('ListaDatos').innerHTML
+
 }
-const deleteContact = ( index ) => {
-  data.splice( index, 1 )
-  localStorage.setItem( DataKeys, JSON.stringify(data))
-  window.location.reload()
+// const UpdateData = (index) => {
+//     const newName = document.getElementById('ListaDatos').value
+//     data[index] = newName
+//     localStorage.setItem(DataKeys, JSON.stringify(data))
+//     window.location.reload()
+// }
+
+const deleteContact = (index) => {
+    data.splice(index, 1)
+    localStorage.setItem(DataKeys, JSON.stringify(data))
+    window.location.reload()
 }
 
-let GetData = () => {
+let UpdateDataList = () => {
+    //agregar clean datalist
     const DataListString = localStorage.getItem(DataKeys)
     data = JSON.parse(DataListString)
     console.log(data);
-    //DataList.innerHTML = "";
+    // ListaDatos.innerHTML = "";
 
-    data.forEach((data, index) => {
-        const nodeName = document.createElement('p')
-        const updateButton = document.createElement('button')
-        updateButton.textContent = `Actualizar ${index + 1}`
-        updateButton.setAttribute('onclick', `UpdateData(${index})`)
+    const DataList = document.getElementById('ListaDatos')
 
+data.forEach((data, index) => {
 
-        const deleteButton = document.createElement('button')
-        deleteButton.textContent = `Eliminar ${index + 1}`
+ const nodeName = document.createElement('p')
+    const updateButton = document.createElement('button')
+    updateButton.textContent = `Actualizar ${index + 1}`
+    updateButton.setAttribute('onclick', `UpdateData(${index})`)
 
-        deleteButton.setAttribute('onclick', `deleteContact(${index})`)
-        nodeName.textContent = `${index + 1}: ${data}`
-        DataList.appendChild(nodeName)
-        DataList.appendChild(updateButton)
-        DataList.appendChild(deleteButton)
-    });
+    const deleteButton = document.createElement('button')
+    deleteButton.textContent = `Eliminar ${index + 1}`
+
+    deleteButton.setAttribute('onclick', `deleteContact(${index})`)
+    nodeName.textContent = `${index + 1}: ${data}`
+    DataList.appendChild(nodeName)
+    DataList.appendChild(updateButton)
+    DataList.appendChild(deleteButton)
+   });
 };
 
 
 // Cargar los datos del localstorage
 
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('data') == null) {
+        localStorage.setItem('data', JSON.stringify([]));
+    } else {
+        DataList = JSON.parse(localStorage.getItem('data'));
+        UpdateDataList();
+    }
+});
 
 
